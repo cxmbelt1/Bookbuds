f58a4ab0acae880ce7adc270616d84237189b1fa
dishymilo <cabz2003@gmail.com>
2023-10-06 15:02:29 -0500

    Update Prototipo 1.py

--- Prototipo 1.py
+++ Prototipo 1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import time
+import matplotlib.pyplot as plt
 
 class Nodo:
     def __init__(self, libro):
@@ -54,7 +55,7 @@ class ListaLibros:
 
 def leer_libros_excel(archivo):
     df = pd.read_excel(archivo)
-    return df['books'].tolist()
+    return df['books'].astype(str).tolist()
 
 def menu():
     print("\n--- Menú ---")
@@ -64,14 +65,51 @@ def menu():
     print("4. Imprimir lista de libros")
     print("5. Buscar todos los libros y calcular tiempo promedio")
     print("6. Salir")
+    print("7. Eliminar todos los libros y calcular tiempo promedio")
     opcion = input("Seleccione una opción: ")
     return opcion
 
 if __name__ == "__main__":
     lista = ListaLibros()
+    
     libros = leer_libros_excel("10k.xlsx")
+    
+    # Listas para almacenar los tiempos de cada operación de agregar libro y los nombres de los libros
+    add_times = []
+    libro_names = []
+    
     for libro in libros:
+        # Inicio del tiempo de adición de un libro
+        start_time = time.time()
+        
         lista.agregar_libro(libro)
+        
+        # Fin del tiempo de adición de un libro
+        end_time = time.time()
+        
+        # Cálculo del tiempo utilizado para añadir un libro
+        add_time = end_time - start_time
+        add_times.append(add_time)
+        libro_names.append(libro)
+        
+        print(f"Tiempo para añadir el libro '{libro}': {add_time:.8f} segundos")
+    
+    # Cálculo del tiempo promedio utilizado para añadir un libro
+    avg_time = sum(add_times) / len(add_times) if add_times else 0
+    
+    print(f"\nTiempo promedio para añadir un libro: {avg_time:.8f} segundos")
+    
+    # Gráfica de los tiempos de adición de cada libro
+    plt.plot(libro_names, add_times, marker='o', linestyle='', color='b', label='Tiempo de adición')
+    plt.axhline(y=avg_time, color='r', linestyle='--', label='Tiempo promedio')
+    plt.xlabel('Libro')
+    plt.ylabel('Tiempo (s)')
+    plt.title('Tiempos de Adición de Libros')
+    plt.legend()
+    plt.xticks([])
+    plt.tight_layout()
+    plt.show()
+    
 
     while True:
         opcion = menu()
@@ -95,21 +133,66 @@ if __name__ == "__main__":
         elif opcion == "5":
             total_time = 0
             num_searches = len(libros)
+            search_times = []
             
+            start_all_time = time.time()
             for libro in libros:
                 start_time = time.time()
                 lista.buscar_libro(libro)
                 end_time = time.time()
                 search_time = end_time - start_time
+                search_times.append(search_time)
                 total_time += search_time
                 print(f"Tiempo para buscar '{libro}': {search_time:.8f} segundos")
+            end_all_time = time.time()
+            all_search_time = end_all_time - start_all_time
             
             avg_time = total_time / num_searches if num_searches > 0 else 0
             print(f"\nTiempo promedio de búsqueda: {avg_time:.8f} segundos")
+            print(f"Tiempo total para buscar todos los libros: {all_search_time:.8f} segundos")
+            
+            plt.plot(search_times, marker='o', linestyle='', color='b', label='Tiempo de búsqueda')
+            plt.axhline(y=avg_time, color='r', linestyle='--', label='Tiempo promedio')
+            plt.xlabel('Libro')
+            plt.ylabel('Tiempo (s)')
+            plt.title('Tiempos de Búsqueda de Libros')
+            plt.legend()
+            plt.tight_layout()
+            plt.show()
         
         elif opcion == "6":
             print("Saliendo del programa...")
             break
+        
+        elif opcion == "7":
+            total_time = 0
+            num_deletions = len(libros)
+            deletion_times = []
+            
+            start_all_time = time.time()
+            for libro in libros:
+                start_time = time.time()
+                lista.eliminar_libro(libro)
+                end_time = time.time()
+                deletion_time = end_time - start_time
+                deletion_times.append(deletion_time)
+                total_time += deletion_time
+                print(f"Tiempo para eliminar '{libro}': {deletion_time:.8f} segundos")
+            end_all_time = time.time()
+            all_deletion_time = end_all_time - start_all_time
+            
+            avg_time = total_time / num_deletions if num_deletions > 0 else 0
+            print(f"\nTiempo promedio de eliminación: {avg_time:.8f} segundos")
+            print(f"Tiempo total para eliminar todos los libros: {all_deletion_time:.8f} segundos")
+            
+            plt.plot(deletion_times, marker='o', linestyle='', color='b', label='Tiempo de eliminación')
+            plt.axhline(y=avg_time, color='r', linestyle='--', label='Tiempo promedio')
+            plt.xlabel('Libro')
+            plt.ylabel('Tiempo (s)')
+            plt.title('Tiempos de Eliminación de Libros')
+            plt.legend()
+            plt.tight_layout()
+            plt.show()
         
         else:
             print("Opción no válida. Por favor, intente de nuevo.")
