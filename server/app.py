c13597d610901f1523ae90b006c8bf70f4fafe4c
dishymilo <cabz2003@gmail.com>
2023-11-20 15:02:54 -0500

    mmm

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+from Untitled1 import Storage
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,8 +10,18 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    
+    books_df = pd.read_csv('books.csv', on_bad_lines='skip')
+    book_titles = books_df['title'].head(3).tolist()  
+    
+    return render_template("dashboard.html", book_titles=book_titles)
 
+@app.route("/search")
+def search():
+    query = request.args.get('query', '')  
+    storage = Storage()
+    search_results = storage.search_books(query)  
+    return render_template("search_results.html", books=search_results, query=query)
 
 if __name__ == '__main__':
     app.run(debug=True)
