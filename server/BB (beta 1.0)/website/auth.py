0e863cd18836bb7b932dacba2d7d7760a70ea859
dishymilo <cabz2003@gmail.com>
2023-11-20 23:41:06 -0500

    q

--- server/BB (beta 1.0)/website/auth.py
+++ server/BB (beta 1.0)/website/auth.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -30,6 +32,7 @@ def sign_up():
         elif len(password1) <7:
             flash('Password must be at least 7 characters', category='error')
         else:
+            new_user = User(email=email, first_name=firstName, password=password1)
             flash('Account created!', category='success')
 
 
