5f6012f9fa14d5056a2b21eb659067885df6c681
dishymilo <cabz2003@gmail.com>
2023-11-20 20:10:36 -0500

    Update __init__.py

--- server/BB (beta 1.0)/website/__init__.py
+++ server/BB (beta 1.0)/website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -21,3 +22,6 @@ def create_app():
     return app
 
 def create_database(app):
+    if not path('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
