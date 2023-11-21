9ca105e5a5631558f7b06df3e6cde048d1111a2e
dishymilo <cabz2003@gmail.com>
2023-11-20 22:29:22 -0500

    diablo

--- server/BB (beta 1.0)/website/auth.py
+++ server/BB (beta 1.0)/website/auth.py
@@ -2,13 +2,16 @@ from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
+
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
